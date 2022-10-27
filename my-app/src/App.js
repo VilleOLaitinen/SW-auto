5648e7906b9e4ea2434d4de9e680587e72a6e5a8
DozEm99 <79802760+DozEm99@users.noreply.github.com>
2022-10-27 09:33:36 +0300

    Sivu tehty

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://i.ytimg.com/vi/KEkrWRHCDQU/maxresdefault.jpg"></img>
+
+    </div>
+  );
+}
+
+export default App;
