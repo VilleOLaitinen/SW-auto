78020981170ad98d84844a389186787936b7e367
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2022-12-11 23:59:03 +0200

    new version of algorithm

--- backend/python/estimate_price.py
+++ backend/python/estimate_price.py
@@ -1,35 +1,41 @@
-import numpy as np
 
-bensiini, diesel, automaatti, manuaali = 0, 1, 1, 0
+def estimate_price():
+    import json
+    import numpy as np
 
-""" Vuosi, km, polttoaine, moottorin tilavuus dl (lol), hinta """
-training_data = [[2012, 248000, diesel, automaatti, 20, 10400],
-                 [2011, 246000, diesel, automaatti, 20, 9900],
-                 [2011, 254600, diesel, automaatti, 20, 8800],
-                 [2013, 266000, diesel, automaatti, 20, 8900],
-                 [2013, 280000, diesel, automaatti, 20, 9400],
-                 [2011, 285000, diesel, automaatti, 20, 8450],
-                 [2013, 252000, diesel, automaatti, 20, 12300],
-                 [2011, 268000, bensiini, automaatti, 18, 8400],
-                 [2012, 273000, diesel, manuaali, 20, 9200],
-                 [2013, 261000, diesel, manuaali, 20, 8370],
-                 [2012, 245000, diesel, manuaali, 20, 8360],
-                 [2011, 245000, diesel, manuaali, 20, 8880],
-                 [2011, 208000, diesel, automaatti, 20, 8900],
-                 [2013, 238000, diesel, automaatti, 20, 10790],
-                 [2013, 221000, diesel, automaatti, 20, 9750],
-                 [2013, 233000, bensiini, automaatti, 20, 8900],
-                 [2012, 206472, diesel, manuaali, 20, 10900],
-                 [2011, 199000, bensiini, manuaali, 20, 10990],
-                 [2013, 227000, diesel, automaatti, 20, 12390]]
+    # Opening JSON file
+    with open('cars.json') as json_file:
+    	data = json.load(json_file)
 
-# test_data = [[2012, 279000, 1, 1, 20, 100000],
-#             [2012, 236000, 1, 1, 20, 9970]]
+        my_car = data['my_car'][0]
+        p_dict, my_dict = {}, {}
 
-test_data = [[2012, 279000, 1, 1, 20, 100000]]
+        car_list = []
+        training_data, test_data = [], []
+        for i in range(len(data['cars'])):
+            p_dict = data['cars'][i]
+            
+            for key in p_dict:
+                if key in my_car:
+                    if key == 'engine':
+                        litres = float(p_dict[key])
+                        car_list.append(int(litres * 10))
+                    else:
+                        car_list.append(p_dict[key])
+                if key == 'price':
+                    car_list.append(p_dict[key])
+            training_data.append(car_list)
+            car_list = []
 
+        for k in range(len(data['my_car'])):
+            my_dict = data['my_car'][k]
+            for key in my_dict:
+                if key == 'engine':
+                    litres = float(p_dict[key])
+                    test_data.append(int(litres * 10))
+                elif key != 'price':
+                    test_data.append(my_dict[key])
 
-def main():
     """ Create training array"""
     training_list = []
 
@@ -67,7 +73,4 @@ def main():
 
     result = x_testing @ c
 
-    print("Price estimate: ", str(result[0]) + " €")
-
-
-main()
+    return str(result[0])
